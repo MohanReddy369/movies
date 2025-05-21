063b9119c50a7cc9c15f30edc8ad7e4ed0ff8dee
MohanReddy369 <mohanreddy54609@gmail.com>
2025-05-21 14:28:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '8132314177:AAFTvL9OXlkv8-02ewCV_6jpahnM-hV
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7660633418').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/@User_16437") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002393452878'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', ''https://t.me/+dn2RdhYQUVA0Y2Q1')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+dn2RdhYQUVA0Y2Q1')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002456492253').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
