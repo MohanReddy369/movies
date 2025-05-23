d81772254ae7c2eee8d5d8411b77944594e41c00
MohanReddy369 <mohanreddy54609@gmail.com>
2025-05-23 10:34:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = (environ.get('START_IMG', 'https://th.bing.com/th/id/OIP.GxL9VDyE9LYjLCNg0O-KHgHaHa?w=162&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7')).split()
+#START_IMG = (environ.get('START_IMG', 'https://th.bing.com/th/id/OIP.GxL9VDyE9LYjLCNg0O-KHgHaHa?w=162&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7')).split()
 #FORCESUB_IMG = environ.get('FORCESUB_IMG', '')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://envs.sh/_kA.jpg')).split()
